ae51776a8830f045bc9d9dbffd12cf6a78cdb5d3
Abhishek <abhi885990@gmail.com>
2023-01-27 14:20:39 +0530

    added authentication

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,81 @@
-import './styles.css';
+import "./styles.css";
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { onAuthStateChanged } from "firebase/auth";
+import { setDoc, doc, getDoc } from "firebase/firestore";
+import {
+  getAuth,
+  GoogleAuthProvider,
+  signInWithPopup,
+  signOut,
+} from "firebase/auth";
+import { useState, useEffect } from "react";
 
-function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+// Firebase config
+const firebaseConfig = {
+  apiKey: "AIzaSyAjfG8RsO9mgYNdUOD9d_IwBsZAY-LwniU",
+  authDomain: "my-ai-doc.firebaseapp.com",
+  projectId: "my-ai-doc",
+  storageBucket: "my-ai-doc.appspot.com",
+  messagingSenderId: "855573263280",
+  appId: "1:855573263280:web:ba00427c24e8e9caa5dc75",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+export const db = getFirestore(app);
+
+const provider = new GoogleAuthProvider();
+
+const signInWithGoogle = () => {
+  signInWithPopup(auth, provider);
+};
+const signOutFromGoogle = () => {
+  signOut(auth, provider);
+};
+
+function MyApp({ Component, pageProps }) {
+  const [user, setUser] = useState(null);
+  // const dispatch = useDispatch();
+  const addNewUser = async (uid) => {
+    const userRef = doc(db, "users", uid);
+
+    const data = {
+      promptsList: [],
+    };
+    const docSnap = await getDoc(userRef);
+    if (docSnap.exists()) {
+      console.log("Document already exist");
+    } else {
+      console.log("No such document!");
+      setDoc(userRef, data)
+        .then(() => {
+          console.log("Document has been added successfully");
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }
+  };
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        console.log("user is", user);
+        setUser(user);
+        addNewUser(user.uid);
+      } else {
+        setUser(null);
+      }
+    });
+  }, []);
+  return (
+    <Component
+      {...pageProps}
+      user={user}
+      signIn={signInWithGoogle}
+      signOut={signOutFromGoogle}
+    />
+  );
 }
-export default App;
+export default MyApp;
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,16 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Head from "next/head";
-import Image from "next/image";
-import buildspaceLogo from "../assets/buildspace-logo.png";
+import GoogleButton from "react-google-button";
 import Button from "@mui/material/Button";
+import ChatBubbleOutlineIcon from "@mui/icons-material/ChatBubbleOutline";
+import CancelIcon from "@mui/icons-material/Cancel";
+import IconButton from "@mui/material/IconButton";
 
-const Home = () => {
+const Home = (props) => {
   const [userInput, setUserInput] = useState("");
   const [apiOutput, setApiOutput] = useState("");
   const [isGenerating, setIsGenerating] = useState(false);
+  const [searchedPrompts, setSearchedPrompts] = useState([]);
 
   const callGenerateEndpoint = async () => {
     setIsGenerating(true);
@@ -23,10 +26,17 @@ const Home = () => {
 
     const data = await response.json();
     const { output } = data;
-    console.log("OpenAI replied...", output.text);
 
     setApiOutput(`${output.text}`);
     setIsGenerating(false);
+    let temp = searchedPrompts;
+    temp.unshift({
+      id: Date.now(),
+      prompt: userInput,
+      output: `${output.text}`,
+    });
+    if (temp.length > 11) temp.pop();
+    setSearchedPrompts(temp);
   };
   const onUserChangedText = (event) => {
     setUserInput(event.target.value);
@@ -35,82 +45,118 @@ const Home = () => {
     setApiOutput("");
     setUserInput("");
   };
+  const handleDelete = (id) => {
+    let updatedPrompts = searchedPrompts.filter((m) => m.id != id);
+    setSearchedPrompts(updatedPrompts);
+  };
+  const handleUpdatePromptBox = (id) => {
+    let promptObj = searchedPrompts.filter((m) => m.id === id);
+    setUserInput(promptObj[0].prompt);
+    setApiOutput(promptObj[0].output);
+  };
+
   return (
     <div className="root">
-      <Head>
-        <title>GPT-3 Writer | buildspace</title>
-      </Head>
-      <div className="container">
-        <div className="header">
-          <div className="header-title">
-            <h1>Your AI Doctor</h1>
-          </div>
-          <div className="header-subtitle">
-            <h2>Tell me about your symptoms</h2>
-          </div>
-        </div>
-        <div className="prompt-container">
-          <textarea
-            className="prompt-box"
-            placeholder="start typing here"
-            value={userInput}
-            onChange={onUserChangedText}
-          />
-          <div className="prompt-buttons">
-            <a
-              className={
-                isGenerating ? "generate-button loading" : "generate-button"
-              }
-              onClick={callGenerateEndpoint}
-            >
-              <div className="generate">
-                {isGenerating ? (
-                  <span className="loader"></span>
-                ) : (
-                  <p>Generate</p>
-                )}
-              </div>
-            </a>
-            <Button
-              variant="outlined"
-              style={{
-                height: 53,
-                width: 100,
-                borderRadius: 30,
-                backgroundColor: "rgb(255, 79, 18)",
-                color: "#ffffff",
-              }}
-              onClick={handleClear}
-            >
-              Clear
-            </Button>
-          </div>
-          {apiOutput && (
-            <div className="output">
-              <div className="output-header-container">
-                <div className="output-header">
-                  <h3>Output</h3>
+      {!props.user ? (
+        <GoogleButton
+          className="signin_btn"
+          onClick={props.signIn}
+        ></GoogleButton>
+      ) : (
+        <>
+          <Head>
+            <title>myaidoc</title>
+          </Head>
+          <Button className="logout_btn" onClick={props.signOut}>
+            Logout
+          </Button>
+          <div className="user-prompts">
+            {searchedPrompts.map((message) => {
+              return (
+                <div className="card">
+                  <div
+                    className="prompt-text"
+                    onClick={() => handleUpdatePromptBox(message.id)}
+                  >
+                    <div>
+                      <ChatBubbleOutlineIcon />
+                    </div>
+                    {message.prompt}{" "}
+                  </div>
+                  <div className="cross-icon">
+                    <IconButton>
+                      <CancelIcon
+                        style={{ color: "red" }}
+                        onClick={() => handleDelete(message.id)}
+                      />
+                    </IconButton>
+                  </div>
                 </div>
+              );
+            })}
+          </div>
+          <div className="container">
+            <div className="header">
+              <div className="header-title">
+                <h1>Your AI Doctor</h1>
               </div>
-              <div className="output-content">
-                <p>{apiOutput}</p>
+              <div className="header-subtitle">
+                <h2>Tell me about your symptoms</h2>
               </div>
             </div>
-          )}
-        </div>
-      </div>
-      <div className="badge-container grow">
-        <a
-          href="https://buildspace.so/builds/ai-writer"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <div className="badge">
-            <Image src={buildspaceLogo} alt="buildspace logo" />
-            <p>build with buildspace</p>
+            <div className="prompt-container">
+              <textarea
+                className="prompt-box"
+                placeholder="start typing here"
+                value={userInput}
+                onChange={onUserChangedText}
+              />
+              <div className="prompt-buttons">
+                <a
+                  className={
+                    isGenerating ? "generate-button loading" : "generate-button"
+                  }
+                  onClick={callGenerateEndpoint}
+                >
+                  <div className="generate">
+                    {isGenerating ? (
+                      <span className="loader"></span>
+                    ) : (
+                      <p>Generate</p>
+                    )}
+                  </div>
+                </a>
+                <Button
+                  variant="outlined"
+                  style={{
+                    height: 53,
+                    width: 100,
+                    borderRadius: 30,
+                    backgroundColor: "rgb(255, 79, 18)",
+                    color: "#ffffff",
+                  }}
+                  onClick={handleClear}
+                >
+                  Clear
+                </Button>
+              </div>
+              {apiOutput && (
+                <div className="output">
+                  <div className="output-header-container">
+                    <div className="output-header">
+                      <h3>Output</h3>
+                    </div>
+                  </div>
+                  <div className="output-content">
+                    <p>{apiOutput}</p>
+                  </div>
+                </div>
+              )}
+            </div>
           </div>
-        </a>
-      </div>
+          <div className="badge-container grow"></div>
+        </>
+      )}
     </div>
   );
 };
